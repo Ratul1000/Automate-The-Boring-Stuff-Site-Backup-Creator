bf133a3812de16a76c6e046f8bf0a573b17cf586
Ratul <jannatulnyeemr@gmail.com>
2025-03-21 18:06:15 +0600

    futher change

--- backup_main_run.py
+++ backup_main_run.py
@@ -17,7 +17,7 @@ def get_page_links(base_link):
     return site_links
 
 
-print(get_page_links('https://automatetheboringstuff.com/#toc'))
+page_links = get_page_links('https://automatetheboringstuff.com/#toc')
 
 '''
 1.Css file is same for everyone. So, first download the file and put it in the backup directory and save its path in variable
@@ -27,3 +27,14 @@ print(get_page_links('https://automatetheboringstuff.com/#toc'))
 5.Save the html file
 
 '''
+backup_directory = Path.cwd() / 'backup'
+os.makedirs(backup_directory, exist_ok=True)
+css_file_path = backup_directory / 'style'
+os.makedirs(css_file_path, exist_ok=True)
+
+css_file = requests.get('https://automatetheboringstuff.com/automate2_website.css')
+css_file.raise_for_status()
+css_file_final_path= css_file_path / 'automate2_website.css'
+
+with open(css_file_final_path, 'wb') as f:
+    f.write(css_file.content)
