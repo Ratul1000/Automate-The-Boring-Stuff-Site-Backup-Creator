bc712a74961b7fcd5ca6504797b61ec12bf6ae99
Ratul <jannatulnyeemr@gmail.com>
2025-03-21 20:11:24 +0600

    Functional. Goes through each page, saves images and the page. Changes the href attributes to point to disk file paths.
    More Improvements :
    1.Make download Faster
    2.Use Threading
    3.Make Toc and Next buttons work

--- backup_main_run.py
+++ backup_main_run.py
@@ -19,6 +19,16 @@ def get_page_links(base_link):
 
 page_links = get_page_links('https://automatetheboringstuff.com/#toc')
 
+def file_downloader(link, path):
+    file = requests.get(link, stream=True)
+    file.raise_for_status()
+    file_on_disk = open(path, 'wb')
+    for chunk in file.iter_content(1000000):
+        file_on_disk.write(chunk)
+
+    print('File Download Successfull')
+
+
 '''
 1.Css file is same for everyone. So, first download the file and put it in the backup directory and save its path in variable
 2.loop through each link and create chapter as directory + image directory inside.
@@ -27,14 +37,43 @@ page_links = get_page_links('https://automatetheboringstuff.com/#toc')
 5.Save the html file
 
 '''
+path_of_css = Path('../style/automate2_website.css')
+
+
 backup_directory = Path.cwd() / 'backup'
 os.makedirs(backup_directory, exist_ok=True)
 css_file_path = backup_directory / 'style'
 os.makedirs(css_file_path, exist_ok=True)
-
-css_file = requests.get('https://automatetheboringstuff.com/automate2_website.css')
-css_file.raise_for_status()
 css_file_final_path= css_file_path / 'automate2_website.css'
 
-with open(css_file_final_path, 'wb') as f:
-    f.write(css_file.content)
+file_downloader('https://automatetheboringstuff.com/automate2_website.css', css_file_final_path)
+
+
+for link in page_links:
+    site = requests.get(link)
+    site.raise_for_status()
+    chapter_path = backup_directory / Path(link).name
+    os.makedirs(chapter_path, exist_ok=True)
+
+    soup = bs4.BeautifulSoup(site.content, 'html.parser')
+    (soup.select('head link')[0]).attrs['href'] = str(Path('../style/automate2_website.css'))
+    image_folder = chapter_path/'images'
+
+    os.makedirs(image_folder)
+
+
+    for link2 in soup.select('img'):
+        image_link = urljoin(link, link2.attrs['src'])
+        image_path = image_folder / (Path(image_link).name)
+
+        link2.attrs['src'] = Path('images') / Path(image_link).name
+        file_downloader(image_link, image_path)
+
+    file = open(chapter_path / (Path(link).name + '.html'), 'wb')
+    
+    file.write(soup.prettify(soup.original_encoding))
+
+    print(Path(link).name + ' Done')
+   
+
+    
